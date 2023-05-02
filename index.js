cd170c513db4d4f8692469d9df3284955e69d7ed
Mickey Vershbow <mickey.vershbow@gmail.com>
2023-05-02 12:45:49 -0700

    boilerplate

---
+++ index.js
@@ -0,0 +1,29 @@
+import puppeteer from "puppeteer";
+
+async function run() {
+  // First, we must launch a browser instance
+  const browser = await puppeteer.launch({
+    // Headless option allows us to disable visible GUI, so the browser runs in the "background"
+    // for development lets keep this to true so we can see what's going on but in
+    // on a server we must set this to true
+    headless: false,
+    // This setting allows us to scrape non-https websites easier
+    ignoreHTTPSErrors: true,
+  });
+  // then we need to start a browser tab
+  let page = await browser.newPage();
+  // and tell it to go to some URL
+  await page.goto(
+    "https://web.archive.org/web/20071229045140/http://www.domelights.com/forums/",
+    {
+      waitUntil: "domcontentloaded",
+    }
+  );
+  // print html content of the website
+  console.log(await page.content());
+  // close everything
+  await page.close();
+  await browser.close();
+}
+
+run();
